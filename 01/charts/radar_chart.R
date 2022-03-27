43d5dbedbdcdd6c9956cd4209c8936555e3a3471
João Palmeiro <joaommpalmeiro@gmail.com>
2022-03-27 23:25:16 +0100

    Update radar_chart.R

--- 01/charts/radar_chart.R
+++ 01/charts/radar_chart.R
@@ -53,6 +53,7 @@ vigia <- df %>%
 vigia
 
 # https://github.com/ricardo-bion/ggradar/blob/master/R/ggradar.R#L69
+# https://github.com/easystats/see/blob/master/R/coord_radar.R
 
 vigia %>%
   select(nome_infraestrutura, mes, resumo_infraestrutura) %>%
