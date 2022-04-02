7daf2f26047ed7f5831827cbb078a3ab3807fc76
João Palmeiro <joaommpalmeiro@gmail.com>
2022-04-02 09:46:53 +0100

    Add first version strip plot

--- 01/charts/strip_chart.R
+++ 01/charts/strip_chart.R
@@ -2,7 +2,9 @@ renv::use(
   "styler@1.7.0",
   "here@1.0.1",
   "tidyverse@1.3.1",
-  "svglite@2.1.0"
+  "svglite@2.1.0",
+  "scico@1.3.0",
+  "scales@1.1.1"
 )
 
 library(readr)
@@ -11,6 +13,9 @@ library(lubridate)
 library(ggplot2)
 library(stringr)
 library(forcats)
+library(scico)
+library(scales)
+library(here)
 
 df <- read_csv(
   "https://raw.githubusercontent.com/dssgPT/Plotting-Good-DSSG/main/desafios/001_Seca_Em_Portugal_SNIRH/snirh_clean.csv",
@@ -43,5 +48,66 @@ missing_vigia %>% glimpse()
 vigia <- vigia %>% na.exclude()
 vigia %>% glimpse()
 
+n_anos <- n_distinct(vigia$ano)
+n_anos
+
+# https://ggplot2.tidyverse.org/articles/ggplot2-specs.html#sec:shape-spec
+# https://wilkelab.org/ungeviz/index.html
+# https://mjskay.github.io/ggdist/
+# https://stackoverflow.com/a/42749526
+# https://ggplot2.tidyverse.org/reference/geom_spoke.html
+# https://github.com/thomasp85/scico
+# https://www.fabiocrameri.ch/colourmaps/
+# https://ggplot2.tidyverse.org/reference/aes_linetype_size_shape.html
+# https://altair-viz.github.io/gallery/strip_plot.html
+
+deg2rad <- function(deg) {
+  (deg * pi) / (180)
+}
+
 vigia %>%
-  ggplot()
+  ggplot(aes(x = ano, y = resumo_infraestrutura)) +
+  # geom_point() +
+  geom_vline(aes(xintercept = ano), colour = "gray") +
+  geom_spoke(
+    angle = 0,
+    radius = 0.5,
+    aes(colour = resumo_infraestrutura),
+    show.legend = FALSE,
+    size = 0.5
+  ) +
+  geom_spoke(
+    angle = deg2rad(180),
+    radius = 0.5,
+    aes(colour = resumo_infraestrutura),
+    show.legend = FALSE,
+    size = 0.5
+  ) +
+  scale_y_continuous(
+    breaks = c(0, 25, 50, 75, 100),
+    limits = c(0, 100),
+    labels = label_percent(scale = 1)
+  ) +
+  scale_colour_scico(
+    palette = "lapaz",
+    limits = c(0, 100),
+    direction = -1
+  ) +
+  theme(
+    plot.margin = margin(t = 0, r = 0, b = 0, l = 0),
+    panel.background = element_rect(fill = NA),
+    axis.ticks = element_blank(),
+    axis.title = element_blank(),
+    axis.text = element_text(
+      size = 12,
+      colour = "black"
+    ),
+    panel.grid.major.y = element_line(
+      colour = "gray",
+      linetype = "dashed"
+    ),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor = element_blank()
+  )
+
+# ggsave(here("strip_plot.svg"))
